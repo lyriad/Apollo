741790c458603178245f0db1cc076dc0b34d2c83
lyriad <manueleduardo0320@gmail.com>
2021-10-20 23:17:50 -0400

    feat: add patients livewire table component

---
+++ app/Http/Livewire/DataTables/PatientsTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\DataTables;
+
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use App\Exports\PatientsExport;
+use Maatwebsite\Excel\Excel;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\Patient;
+
+class PatientsTable extends DataTableComponent
+{
+    public string $primaryKey = 'hid';
+
+    public array $bulkActions = [
+        'exportSelected' => 'Export',
+    ];
+
+    protected string $pageName = 'patients';
+    protected string $tableName = 'patients';
+
+    public function exportSelected()
+    {
+        return (new PatientsExport)->download('patients.csv', Excel::CSV);
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Name', 'name')->searchable(),
+            Column::make('Birthdate', 'birthdate'),
+            Column::make('Age', 'age'),
+            Column::make('Gender', 'gender'),
+            Column::make('WWeight (Kg)', 'weight_kg'),
+            Column::make('Height (Cm)', 'height_cm'),
+        ];
+    }
+
+    public function query(): Builder
+    {
+        return Patient::query();
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,6 @@
 @extends('app')
-@section('app')
-    Home
+@section('slot')
+    <div id="patients-table-container" class="py-8 px-6">
+        <livewire:data-tables.patients-table />
+    </div>
 @endsection
