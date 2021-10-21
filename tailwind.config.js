e1403b03b38aa3d326c03c92d0191463d6aa9230
lyriad <manueleduardo0320@gmail.com>
2021-10-20 23:15:08 -0400

    build: add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  mode: 'jit',
   purge: [
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.vue',
+    './vendor/rappasoft/laravel-livewire-tables/resources/views/tailwind/**/*.blade.php',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -11,5 +13,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
