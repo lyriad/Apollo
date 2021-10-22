d5310f3ab45726a6a6b748b823e87f2c62534b88
lyriad <manueleduardo0320@gmail.com>
2021-10-21 20:37:12 -0400

    build: add tailwind safelist generator package
    add package to generate safelist file with style classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './resources/**/*.js',
     './resources/**/*.vue',
     './vendor/rappasoft/laravel-livewire-tables/resources/views/tailwind/**/*.blade.php',
+    './tailwind-safelist.txt'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -18,5 +19,22 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
+    require('tailwind-safelist-generator')({
+      path: 'tailwind-safelist.txt',
+      patterns: [
+        'p-{spacing}',
+        'px-{spacing}',
+        'py-{spacing}',
+        'pt-{spacing}',
+        'pb-{spacing}',
+        'pl-{spacing}',
+        'pr-{spacing}',
+        'rounded-{borderRadius}',
+        'text-{colors}',
+        'bg-{colors}',
+        'border-{borderWidth}',
+        '{screens}:gap-{spacing}'
+      ],
+    }),
   ],
 }
